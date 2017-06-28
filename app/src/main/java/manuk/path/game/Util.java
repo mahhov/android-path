94b9fda85d92a4bd374cdc327a168e3a0f9d2546
manukfarmers <mahhov@yahoo.com>
2017-06-28 08:42:15 -0700

    clean util set mag max

--- app/src/main/java/manuk/path/game/Util.java
+++ app/src/main/java/manuk/path/game/Util.java
@@ -10,12 +10,11 @@ class Util {
 		return new double[] {x / m, y / m};
 	}
 	
-	static double[] setMagnitudeMax(double x, double y, double mag) { // todo: rewrite this
-		double m = magnitude(x, y);
-		if (m > mag)
-			return new double[] {x / m * mag, y / m * mag};
-		else
-			return new double[] {x, y};
+	static double[] setMagnitudeMax(double x, double y, double mag) {
+		double m = mag / magnitude(x, y);
+		if (m > 1)
+			m = 1;
+		return new double[] {x * m, y * m};
 	}
 	
 	static double minMax(double value, double min, double max) {
