efec2ac43c228b5c16eab06ac8f2136a204bbfbd
Manuk Hovanesian <manuk.hovanesian@farmersinsurance.com>
2017-07-01 19:28:22 -0700

    painter null check

--- app/src/main/java/manuk/path/game/painter/Painter.java
+++ app/src/main/java/manuk/path/game/painter/Painter.java
@@ -22,28 +22,36 @@ public class Painter {
 	public void prep(SurfaceHolder surfaceHolder) {
 		this.surfaceHolder = surfaceHolder;
 		canvas = surfaceHolder.lockCanvas();
+		if (canvas == null)
+			return;
 		canvas.drawColor(OUT_COLOR);
 		drawRect(0, 0, 1, 1, Color.BLACK);
 	}
 	
 	public void post() {
+		if (canvas == null)
+			return;
 		drawRect(0, -1, 1, 1, OUT_COLOR);
 		drawRect(0, 1, 1, 1, OUT_COLOR);
 	}
 	
 	public void end() {
-		surfaceHolder.unlockCanvasAndPost(canvas);
+		if (canvas != null)
+			surfaceHolder.unlockCanvasAndPost(canvas);
 	}
 	
 	public double[] convertFromAbsolute(double x, double y) {
 		return new double[] {(x - Measurements.SCREEN_SHIFT_X) / Measurements.SCREEN_WIDTH + Measurements.SCREEN_SHIFT_X, (y - Measurements.SCREEN_SHIFT_Y) / Measurements.SCREEN_HEIGHT};
 	}
 	
-	public void drawRect(double x, double y, double width, double height, int color, boolean frame) {
+	public void drawRect(double x, double y, double width, double height, int color, int frame) {
+		if (canvas == null)
+			return;
 		paint.setColor(color);
-		if (frame)
+		if (frame != 0) {
 			paint.setStyle(Paint.Style.STROKE);
-		else
+			paint.setStrokeWidth(frame);
+		} else
 			paint.setStyle(Paint.Style.FILL);
 		float left = (float) (Measurements.SCREEN_SHIFT_X + x * Measurements.SCREEN_WIDTH);
 		float top = (float) (Measurements.SCREEN_SHIFT_Y + y * Measurements.SCREEN_HEIGHT);
@@ -53,10 +61,14 @@ public class Painter {
 	}
 	
 	public void drawRect(double x, double y, double width, double height, int color) {
-		drawRect(x, y, width, height, color, false);
+		if (canvas == null)
+			return;
+		drawRect(x, y, width, height, color, 0);
 	}
 	
 	public void drawPolygon(double[] x, double[] y, int color, boolean frame) {
+		if (canvas == null)
+			return;
 		paint.setColor(color);
 		if (frame)
 			paint.setStyle(Paint.Style.STROKE);
@@ -74,14 +86,18 @@ public class Painter {
 	}
 	
 	public void drawText(String text, double x, double y, int color) {
+		if (canvas == null)
+			return;
 		paint.setTextAlign(Paint.Align.LEFT);
 		paint.setColor(color);
 		float left = (float) (Measurements.SCREEN_SHIFT_X + x * Measurements.SCREEN_WIDTH);
 		float top = (float) (Measurements.SCREEN_SHIFT_Y + y * Measurements.SCREEN_HEIGHT);
 		canvas.drawText(text, left, top, paint);
-	}	
+	}
 	
 	public void drawCenteredText(String text, double x, double y, int color) {
+		if (canvas == null)
+			return;
 		paint.setTextAlign(Paint.Align.CENTER);
 		paint.setColor(color);
 		float left = (float) (Measurements.SCREEN_SHIFT_X + x * Measurements.SCREEN_WIDTH);
