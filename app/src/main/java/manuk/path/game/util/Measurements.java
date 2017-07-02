583cdbc0a1dbf77123436b5fea28457c23d0a82f
Manuk Hovanesian <manuk.hovanesian@farmersinsurance.com>
2017-07-01 18:11:39 -0700

    refactor Measurements and make full screen

--- app/src/main/java/manuk/path/game/util/Measurements.java
+++ app/src/main/java/manuk/path/game/util/Measurements.java
@@ -1,24 +1,31 @@
 package manuk.path.game.util;
 
 public class Measurements {
-	private static final boolean OVERVIEW_MODE = false;
-	
 	public static final int MAP_WIDTH = 100, MAP_LENGTH = MAP_WIDTH, MAP_HEIGHT = 3;
-	private static final int VIEW_RATIO = 1;
-	private static final int VIEW_WIDTH = 30 * (OVERVIEW_MODE ? 3 : 1), VIEW_HEIGHT = VIEW_WIDTH * VIEW_RATIO;
 	
-	public static int SCALED_VIEW_WIDTH = VIEW_WIDTH, SCALED_VIEW_HEIGHT = VIEW_HEIGHT;
-	public static double SCALED_BLOCK_WIDTH = 1. / SCALED_VIEW_WIDTH, SCALED_BLOCK_HEIGHT = 1. / SCALED_VIEW_HEIGHT;
+	private static double VIEW_RATIO;
+	private static int VIEW_WIDTH, VIEW_HEIGHT;
+	
+	public static int SCALED_VIEW_WIDTH, SCALED_VIEW_HEIGHT;
+	public static double SCALED_BLOCK_WIDTH, SCALED_BLOCK_HEIGHT;
 	
 	public static int SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_SHIFT_X, SCREEN_SHIFT_Y;
 	
-	public static final double VIEW_STRETCH_Z = SCALED_BLOCK_WIDTH / 10 * (OVERVIEW_MODE ? 0 : 1);
+	public static double VIEW_STRETCH_Z;
 	
 	public static void init(int fullWidth, int fullHeight) {
+		VIEW_RATIO = 1. * fullHeight / fullWidth;
+		VIEW_WIDTH = 30;
+		VIEW_HEIGHT = (int) (VIEW_WIDTH * VIEW_RATIO);
+		
+		setScale(1);
+		
 		SCREEN_WIDTH = fullWidth;
-		SCREEN_HEIGHT = fullWidth * VIEW_RATIO;
+		SCREEN_HEIGHT = (int) (fullWidth * VIEW_RATIO);
 		SCREEN_SHIFT_X = 0;
 		SCREEN_SHIFT_Y = (fullHeight - SCREEN_HEIGHT) / 2;
+		
+		VIEW_STRETCH_Z = SCALED_BLOCK_WIDTH / 10;
 	}
 	
 	public static void setScale(double scale) {
