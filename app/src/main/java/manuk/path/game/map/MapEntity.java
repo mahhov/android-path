c5505f6bd4da16373f828c17bfe3887376933b2f
manukfarmers <mahhov@yahoo.com>
2017-07-09 08:51:21 -0700

    dynamic max entity size

--- app/src/main/java/manuk/path/game/map/MapEntity.java
+++ app/src/main/java/manuk/path/game/map/MapEntity.java
@@ -1,13 +1,14 @@
 package manuk.path.game.map;
 
 import manuk.path.game.util.LList;
+import manuk.path.game.util.Math3D;
 
 public abstract class MapEntity {
 	private static long count = 0;
 	public static final int ENTITY_LAYERS_COUNT = 4;
 	protected static final int ENTITY_LAYER_FRIENDLY_PROJECTILE = 0, ENTITY_LAYER_HOSTILE_PROJECTILE = 1, ENTITY_LAYER_FRIENDLY_CHARACTER = 2, ENTITY_LAYER_HOSTILE_CHARACTER = 3;
 	public static final boolean[][] ENTITY_COLLISION;
-	public static final double MAX_ENTITY_SIZE = 1;
+	public static double MAX_ENTITY_SIZE = 0;
 	
 	static {
 		ENTITY_COLLISION = new boolean[ENTITY_LAYERS_COUNT][ENTITY_LAYERS_COUNT];
@@ -30,8 +31,7 @@ public abstract class MapEntity {
 		id = count++;
 		this.layer = layer;
 		this.size = size;
-		if (size > MAX_ENTITY_SIZE)
-			System.out.println("WARNING, size " + size + " exceeds MAX_ENTITY_SIZE " + MAX_ENTITY_SIZE);
+		MAX_ENTITY_SIZE = Math3D.max(size, MAX_ENTITY_SIZE);
 	}
 	
 	public abstract void draw(double scrollX, double scrollY);
