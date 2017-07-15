9896b9751bce9d3bc9f0470f496d327697af4ee2
manukfarmers <mahhov@yahoo.com>
2017-07-14 20:14:43 -0700

    larger map size

--- app/src/main/java/manuk/path/game/util/Measurements.java
+++ app/src/main/java/manuk/path/game/util/Measurements.java
@@ -1,7 +1,7 @@
 package manuk.path.game.util;
 
 public class Measurements {
-	public static final int MAP_WIDTH = 100, MAP_LENGTH = MAP_WIDTH, MAP_HEIGHT = 3;
+	public static final int MAP_WIDTH = 300, MAP_LENGTH = MAP_WIDTH, MAP_HEIGHT = 3;
 	
 	private static double VIEW_RATIO;
 	private static int VIEW_WIDTH, VIEW_HEIGHT;
@@ -14,7 +14,7 @@ public class Measurements {
 	
 	public static void init(int fullWidth, int fullHeight) {
 		VIEW_RATIO = 1. * fullHeight / fullWidth;
-		VIEW_WIDTH = 30;
+		VIEW_WIDTH = 50;
 		VIEW_HEIGHT = (int) (VIEW_WIDTH * VIEW_RATIO);
 		
 		setScale(.5);
@@ -32,6 +32,6 @@ public class Measurements {
 		SCALED_VIEW_HEIGHT = (int) viewHeight;
 		SCALED_BLOCK_WIDTH = 1. / viewWidth;
 		SCALED_BLOCK_HEIGHT = 1. / viewHeight;
-		VIEW_STRETCH_Z = SCALED_BLOCK_WIDTH / (10 * scale);
+		VIEW_STRETCH_Z = SCALED_BLOCK_WIDTH / (20 * scale);
 	}
 }
