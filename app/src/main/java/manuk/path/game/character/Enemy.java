8c26b9a7c0feec7e7a607c2508a55e8310aa2f23
Manuk Hovanesian <manuk.hovanesian@farmersinsurance.com>
2017-07-16 09:22:37 -0700

    refactor enemy inheritance, melee enemies only damage if player stays close

--- app/src/main/java/manuk/path/game/character/Enemy.java
+++ app/src/main/java/manuk/path/game/character/Enemy.java
@@ -3,17 +3,35 @@ package manuk.path.game.character;
 import manuk.path.game.item.Item;
 import manuk.path.game.map.Map;
 import manuk.path.game.projectile.Projectile;
+import manuk.path.game.util.IntersectionFinder;
 import manuk.path.game.util.LList;
+import manuk.path.game.util.Math3D;
+
+import static manuk.path.game.util.Math3D.setMagnitude;
 
 public abstract class Enemy extends Character {
 	public static final int ENEMY_TYPE_COUNT = 2;
 	public static final int ENEMY_TYPE_MELEE = 0, ENEMY_TYPE_PROJECTILE = 1;
 	
-	Enemy(int layer, double spawnX, double spawnY, int color, double moveSpeed, int attackTime, double maxLife, double maxStamina, int staminaRegenRate, int staminaRegenDelay) {
-		super(layer, spawnX, spawnY, color, moveSpeed, attackTime, maxLife, maxStamina, staminaRegenRate, staminaRegenDelay);
-	}
+	private final double WANDER_THRESHOLD;
+	private final double WANDER_DISTANCE;
+	private final double ACTIVE_DISTANCE;
+	private final double DAMAGE_RANGE;
+	private final double PATH_FIND_FRICTION;
+	private final double ITEM_DROP_RATE;
 	
-	public abstract boolean update(Player player, Map map, LList<Projectile> projectile, LList<Item> item);
+	private double[] awayFromIntersection;
+	
+	Enemy(int layer, double spawnX, double spawnY, int color, double moveSpeed, int attackTime, double maxLife, double wanderThreshold, double wanderDistance, double activeDistance, double damageRange, double pathFindFriction, double itemDropRate) {
+		super(layer, spawnX, spawnY, color, moveSpeed, attackTime, maxLife, 0, 0, 0);
+		WANDER_THRESHOLD = wanderThreshold;
+		WANDER_DISTANCE = wanderDistance;
+		ACTIVE_DISTANCE = activeDistance;
+		DAMAGE_RANGE = damageRange;
+		PATH_FIND_FRICTION = pathFindFriction;
+		ITEM_DROP_RATE = itemDropRate;
+		awayFromIntersection = new double[2];
+	}
 	
 	public static Enemy create(int x, int y, int type) {
 		switch (type) {
@@ -25,5 +43,55 @@ public abstract class Enemy extends Character {
 				System.out.println("UNRECOGNIZED ENEMY TYPE");
 				return new MeleeEnemy(x, y);
 		}
+	}
+	
+	// return true if need to be removed
+	public boolean update(Player player, Map map, LList<Projectile> projectile, LList<Item> item) {
+		if (updateAttack())
+			doAttack(player, projectile);
+		
+		double distance = Math3D.magnitude(player.x - x, player.y - y);
+		if (distance < DAMAGE_RANGE)
+			beginAttack(0);
+		else if (distance < ACTIVE_DISTANCE)
+			chasePlayer(player, map);
+		else if (Math3D.random() > WANDER_THRESHOLD) {
+			wander(map);
+		}
+		if (life <= 0) {
+			die(player, map, item);
+			return true;
+		}
+		return false;
+	}
+	
+	void doAttack(Player player, LList<Projectile> projectile) {
+	}
+	
+	private void chasePlayer(Player player, Map map) {
+		double[] toPlayer = Math3D.setMagnitude(player.x - x, player.y - y, 1);
+		moveDeltaX = toPlayer[0] + awayFromIntersection[0];
+		moveDeltaY = toPlayer[1] + awayFromIntersection[1];
+		awayFromIntersection[0] *= PATH_FIND_FRICTION;
+		awayFromIntersection[1] *= PATH_FIND_FRICTION;
+		handleEnemyIntersection(moveByDir(map));
+	}
+	
+	private void wander(Map map) {
+		goalX = x + Math3D.random(-WANDER_DISTANCE, WANDER_DISTANCE);
+		goalY = y + Math3D.random(-WANDER_DISTANCE, WANDER_DISTANCE);
+		handleEnemyIntersection(moveToGoal(map));
+	}
+	
+	private void die(Player player, Map map, LList<Item> item) {
+		map.removeEntity(this);
+		if (Math3D.random() < ITEM_DROP_RATE)
+			item.addHead(new Item(x, y));
+		player.gainExp(5);
+	}
+	
+	private void handleEnemyIntersection(IntersectionFinder.Intersection intersection) {
+		if (intersection != null && intersection.state == IntersectionFinder.Intersection.COLLISION_ENTITY && intersection.entityCollisionLayer == ENTITY_LAYER_HOSTILE_CHARACTER)
+			awayFromIntersection = setMagnitude(x - intersection.entityCollisionX, y - intersection.entityCollisionY, 1);
 	}
 }
--- app/src/main/java/manuk/path/game/character/MeleeEnemy.java
+++ app/src/main/java/manuk/path/game/character/MeleeEnemy.java
@@ -1,58 +1,27 @@
 package manuk.path.game.character;
 
 import android.graphics.Color;
-import manuk.path.game.item.Item;
-import manuk.path.game.map.Map;
 import manuk.path.game.map.MapEntity;
 import manuk.path.game.projectile.Projectile;
-import manuk.path.game.util.IntersectionFinder;
 import manuk.path.game.util.LList;
 import manuk.path.game.util.Math3D;
 
-import static manuk.path.game.util.Math3D.setMagnitude;
-
-public class MeleeEnemy extends Enemy {
+class MeleeEnemy extends Enemy {
 	private static final double WANDER_THRESHOLD = .98, WANDER_DISTANCE = 5, ACTIVE_DISTANCE = 10, DAMAGE_RANGE = 2;
 	private static final double PATH_FIND_FRICTION = .8;
 	private static final double ITEM_DROP_RATE = .5;
-	private double[] awayFromIntersection;
 	
-	public MeleeEnemy(double spawnX, double spawnY) {
-		super(MapEntity.ENTITY_LAYER_HOSTILE_CHARACTER, spawnX, spawnY, Color.RED, .05, 10, 10, 0, 0, 0);
-		awayFromIntersection = new double[2];
+	private static final int COLOR = Color.RED;
+	private static final double MOVE_SPEED = .05, MAX_LIFE = 10;
+	private static final int ATTACK_TIME = 10;
+	
+	MeleeEnemy(double spawnX, double spawnY) {
+		super(MapEntity.ENTITY_LAYER_HOSTILE_CHARACTER, spawnX, spawnY, COLOR, MOVE_SPEED, ATTACK_TIME, MAX_LIFE, WANDER_THRESHOLD, WANDER_DISTANCE, ACTIVE_DISTANCE, DAMAGE_RANGE, PATH_FIND_FRICTION, ITEM_DROP_RATE);
 	}
 	
-	// return true if need to be removed
-	public boolean update(Player player, Map map, LList<Projectile> projectile, LList<Item> item) {
-		if (updateAttack())
-			player.takeDamage(1);
+	void doAttack(Player player, LList<Projectile> projectile) {
 		double distance = Math3D.magnitude(player.x - x, player.y - y);
-		if (distance < DAMAGE_RANGE)
-			beginAttack(0);
-		else if (distance < ACTIVE_DISTANCE) {
-			double[] toPlayer = Math3D.setMagnitude(player.x - x, player.y - y, 1);
-			moveDeltaX = toPlayer[0] + awayFromIntersection[0];
-			moveDeltaY = toPlayer[1] + awayFromIntersection[1];
-			awayFromIntersection[0] *= PATH_FIND_FRICTION;
-			awayFromIntersection[1] *= PATH_FIND_FRICTION;
-			handleEnemyIntersection(moveByDir(map));
-		} else if (Math3D.random() > WANDER_THRESHOLD) {
-			goalX = x + Math3D.random(-WANDER_DISTANCE, WANDER_DISTANCE);
-			goalY = y + Math3D.random(-WANDER_DISTANCE, WANDER_DISTANCE);
-			handleEnemyIntersection(moveToGoal(map));
-		}
-		if (life <= 0) {
-			map.removeEntity(this);
-			if (Math3D.random() < ITEM_DROP_RATE)
-				item.addHead(new Item(x, y));
-			player.gainExp(5);
-			return true;
-		}
-		return false;
-	}
-	
-	private void handleEnemyIntersection(IntersectionFinder.Intersection intersection) {
-		if (intersection != null && intersection.state == IntersectionFinder.Intersection.COLLISION_ENTITY && intersection.entityCollisionLayer == ENTITY_LAYER_HOSTILE_CHARACTER)
-			awayFromIntersection = setMagnitude(x - intersection.entityCollisionX, y - intersection.entityCollisionY, 1);
+		if (distance < DAMAGE_RANGE * 2)
+			player.takeDamage(1);
 	}
 }
